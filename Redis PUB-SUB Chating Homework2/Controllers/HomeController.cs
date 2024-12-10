e9d7658960df3e0e6ec6a52829f41ca287fb9f7a
Hemze_jx83@itstep.edu.az <Hemze_jx83@itstep.edu.az>
2024-12-10 11:45:35 +0400

    Add project files.

---
+++ Redis PUB-SUB Chating Homework2/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Redis_PUB_SUB_Chating_Homework2.Models;
+using System.Diagnostics;
+
+namespace Redis_PUB_SUB_Chating_Homework2.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        //var muxer = ConnectionMultiplexer.Connect(
+        //    new ConfigurationOptions
+        //    {
+        //        EndPoints = { { "redis-13898.c261.us-east-1-4.ec2.redns.redis-cloud.com", 13898 } },
+        //        User = "default",
+        //        Password = "Bjc65AaWcTrZ9JC1e3OLamCFZP6FeCY4"
+        //    }
+        //);
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SubmitName(string name)
+        {
+            // Name değerini alır ve işlem yapar
+            ViewBag.Message = $"Received name: {name}";
+            return View("Index"); // Gerekirse başka bir View'e yönlendirin
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Redis PUB-SUB Chating Homework2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Redis_PUB_SUB_Chating_Homework2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
